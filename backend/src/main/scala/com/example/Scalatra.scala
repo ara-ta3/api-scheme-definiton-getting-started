18341d000a4931fc0cf47612db1da4dadd7352f7
Arata Tanaka <2089153+ara-ta3@users.noreply.github.com>
2024-10-26 23:35:01 +0900

    Use jackson

--- backend/src/main/scala/com/example/Scalatra.scala
+++ backend/src/main/scala/com/example/Scalatra.scala
@@ -1,12 +1,18 @@
 package com.example
 
 import org.scalatra._
+import org.scalatra.json._
+import org.json4s.{DefaultFormats, Formats}
+import org.openapitools.server.model.*
 
-class Scalatra extends ScalatraServlet {
+class Scalatra extends ScalatraServlet with JacksonJsonSupport  {
+  implicit val formats: Formats = Serialization.formats(NoTypeHints)
 
   // GETリクエストを処理する
   get("/") {
-    "Hello, Scalatra Jakarta!"
+    User(
+      1, "foo", "foo@example.com"
+    )
   }
 
   // POSTリクエストを処理する
