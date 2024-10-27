1e0281f0f0ac02cd0102baf863f66ed37e0ad531
Arata Tanaka <2089153+ara-ta3@users.noreply.github.com>
2024-10-27 22:53:27 +0900

    Fix POST method

--- backend/src/main/scala/com/example/Scalatra.scala
+++ backend/src/main/scala/com/example/Scalatra.scala
@@ -2,6 +2,7 @@ package com.example
 
 import org.scalatra._
 import org.scalatra.json._
+import org.json4s.jvalue2extractable
 import org.json4s.{DefaultFormats, Formats}
 import com.example.api.model._
 
@@ -11,21 +12,29 @@ class Scalatra extends ScalatraServlet
   {
   override implicit val jsonFormats: Formats = DefaultFormats
 
-  get("/api/users") {
+  private var users: Seq[User] = Seq(
+    User(
+      1, "foo", "foo@example.com"
+    )
+  )
+
+  before() {
+    contentType = formats("json")
     response.setHeader("Access-Control-Allow-Origin", "*")
     response.setHeader("Access-Control-Allow-Headers", "Content-Type")
-    contentType = formats("json")
-    Seq(
-      User(
-        1, "foo", "foo@example.com"
-      )
-    )
+  }
+
+  get("/api/users") {
+    users
   }
 
   // POSTリクエストを処理する
   post("/api/users") {
-    val name = params.getOrElse("name", "Anonymous")
-    s"Hello, $name!"
+    val create = parsedBody.extract[CreateUser]
+    val id = users.map(_.id).maxOption.getOrElse(0) + 1
+    val u = User(id, create.name, create.email)
+    users = users :+ u
+    u
   }
 }
 
